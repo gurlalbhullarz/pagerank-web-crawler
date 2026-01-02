4a066e28f1fed06a577a93455bec8478e79246e1
gurlalbhullarz <123107243+gurlalbhullarz@users.noreply.github.com>
2026-01-02 09:55:19 +0530

    Update spjson.py

--- spjson.py
+++ spjson.py
@@ -13,26 +13,26 @@ ORDER BY new_rank DESC
 count = input('How many nodes to visualize: ')
 count = int(count) if count.isdigit() else 20
 
+nodes = list()
+links = list()
+
 rows = cur.fetchmany(count)
 node_ids = [row[0] for row in rows]
 
 min_rank = min(row[2] for row in rows)
 max_rank = max(row[2] for row in rows)
 
-nodes = []
-links = []
-
-for id, url, rank, inbound in rows:
+for row in rows:
     nodes.append({
-        "id": id,
-        "label": url,
-        "value": (rank - min_rank) / (max_rank - min_rank + 0.0001)
+        "id": row[0],
+        "label": row[1],
+        "value": (row[2] - min_rank) / (max_rank - min_rank + 0.0001)
     })
 
 cur.execute('SELECT from_id, to_id FROM Links')
-for from_id, to_id in cur:
-    if from_id in node_ids and to_id in node_ids:
-        links.append({"source": from_id, "target": to_id})
+for row in cur:
+    if row[0] in node_ids and row[1] in node_ids:
+        links.append({"source": row[0], "target": row[1]})
 
 with open('spider.js', 'w') as f:
     f.write('var nodes = ' + str(nodes) + ';\n')
